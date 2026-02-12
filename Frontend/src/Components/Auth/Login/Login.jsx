b0931f4622533f5bcbe0b5f3aba8aa266751c26b
Praveen Kumar Painkra <pk.painkra5@gmail.com>
2026-02-12 23:55:48 +0530

    Style changes in login file

--- Frontend/src/Components/Auth/Login/Login.jsx
+++ Frontend/src/Components/Auth/Login/Login.jsx
@@ -20,7 +20,8 @@ function Login() {
   <div className="w-full max-w-sm p-6 bg-black text-white">
     <h1 className="text-2xl text-4xl font-bold ">Sign In</h1>
     <p className="mb-6 text-gray-400 text-2xl">
-      New user? <NavLink to="/auth/signup" className="text-orange-500 hover:underline">Create an account</NavLink>
+      New user? 
+      <NavLink to="/auth/signup" className="text-orange-500 hover:underline">Create an account</NavLink>
     </p>
     <form>
       <label htmlFor="phone" className="block mb-2 font-medium">Email</label>
