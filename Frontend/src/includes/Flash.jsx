1e27ff8b794e8803d8120516444cf63a49da5e7b
Praveen Kumar Painkra <pk.painkra5@gmail.com>
2026-03-19 21:13:00 +0530

    Delete Frontend directory

--- Frontend/src/includes/Flash.jsx
+++
@@ -1,9 +0,0 @@
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
-
-export default function Flash({flashData}) {
-  return (
-      <Alert severity={flashData.type} className='absolute top-5 right-8'>{flashData.message}</Alert>
-
-  );
-}
